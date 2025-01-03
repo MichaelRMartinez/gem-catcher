5c85f7fa71bd1aec57dd29a484fe139fb0c26f98
Michael <mirima92@gmail.com>
2025-01-03 00:45:42 -0500

    Add user input controls and make paddle move left and right

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+@export var speed : float = 100.0
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	position.y += 100.0 * delta
+	position.y += speed * delta
 	
 	if position.y > get_viewport_rect().size.y:
 		set_process(false)
